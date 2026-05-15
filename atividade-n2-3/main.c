e770b7d7013e952b3ee749f44da6d7ba128296d9
Caio de Santana <caiodesantanaalves@gmail.com>
2026-05-14 22:14:54 -0300

    Update main.c

--- atividade-n2-3/main.c
+++ atividade-n2-3/main.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
-#include "recursao.h"
-#include "interface.h"
+#include "recursao.h"   
+#include "interface.h"  
 
 int main() {
-    int opcao, valor, base, exp;
+    int opcao, n, base, exp;
+
     do {
         exibirMenu();
-        opcao = lerOpcao();
-        switch(opcao) {
+        opcao = lerInteiro("");
+
+        switch (opcao) {
             case 1:
-                printf("Digite o numero decimal: ");
-                scanf("%d", &valor);
-                printf("Binario: ");
-                decToBin(valor); 
+                n = lerInteiro("Digite um numero decimal: ");
+                printf("Resultado em Binario: ");
+                if (n == 0) printf("0");
+                else decToBin(n); 
                 printf("\n");
                 break;
-            case 0:
-                printf("Saindo...\n");
+
+            case 2:
+                base = lerInteiro("Digite a base: ");
+                exp = lerInteiro("Digite o expoente (>=0): ");
+                printf("Resultado da Potencia: %d\n", potencia(base, exp)); 
                 break;
+
+            case 3:
+                n = lerInteiro("Digite um numero inteiro: ");
+                printf("Soma dos digitos: %d\n", somaDigitos(n)); 
+                break;
+
+            case 0:
+                printf("Encerrando o sistema CUCC...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
         }
-    } while(opcao != 0);
+    } while (opcao != 0);
 
     return 0;
 }
